1a84e5b5244cb6d77ed6cce7ac8873703752935d
fabiano230999999999 <134230769+fabiano230999999999@users.noreply.github.com>
2024-09-12 11:16:10 -0600

    medicamento

--- KIVO/Models/Medicamento.cs
+++ KIVO/Models/Medicamento.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,8 @@ namespace KIVO.Models
 {public class Medicamento
 {
     public int Id { get; set; }
+        [Required(ErrorMessage ="el {0} del medicamento es requerido")] 
+        [StringLength(maximumLength:60,MinimumLength = 30,ErrorMessage = "El {0} de el medicamento no cumple con los limites de caracteres establecidos.")]
     public string? Nombre { get; set; }
     public string? Concentracion { get; set; }
     
