e6be2c1604c5263f4d94219d9616997725436737
fabiano230999999999 <134230769+fabiano230999999999@users.noreply.github.com>
2024-09-12 11:07:07 -0600

    ciudad y departamento ya

--- KIVO/Models/Cuidad.cs
+++ KIVO/Models/Cuidad.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,8 @@ namespace KIVO.Models
      public class Cuidad
     {
         public int Id { get; set; } // Primary Key
+        [Required(ErrorMessage ="El {0} de la ciudad es requerido")] 
+        [StringLength(maximumLength:50,MinimumLength = 25,ErrorMessage = "El nombre de su {0} no cumple con los limites de caracteres establecidos.")]
         public string Nombre { get; set; } = null!;
 
         // anclas
--- KIVO/Models/Departamento.cs
+++ KIVO/Models/Departamento.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,8 @@ namespace KIVO.Models
     public class Departamento
     {
         public int Id { get; set; } // Primary Key
+        [Required(ErrorMessage ="Su {0} es requerido")] 
+        [StringLength(maximumLength:60,MinimumLength = 30,ErrorMessage = "El nombre de su {0} no cumple con los limites de caracteres establecidos.")]
         public string Nombre { get; set; } = null!;
 
         // anclas  
